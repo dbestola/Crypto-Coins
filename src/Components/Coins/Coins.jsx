14b47949de0b2d923b9350109cd6d5992f3491e1
OLANREWAJU Oladimeji Nathaniel <olanrewajuoladimeji5@gmail.com>
2024-08-20 14:29:28 +0100

    update

--- src/Components/Coins/Coins.jsx
+++ src/Components/Coins/Coins.jsx
@@ -1,30 +1,33 @@
-import React, { useEffect } from 'react'
+import React, {useState , useEffect } from 'react'
 import './coins.css'
 import axios from 'axios'
 import Coinsitem from './Coinsitem'
-import { useState } from 'react'
 
-const Url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false' 
+
+const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false'
 
 const Coins = () => {
 
     const [coins, setCoin] = useState([])
 
-  
+    const handleCoinfetch = () => {
+        axios.get(url).then((res) => {
+            console.log(res);
+            setCoin(res.data)
+            console.log(coins);
+        }).catch((error) => {
+            console.log(error);
+        })
+    }
 
-useEffect(() => {
-    axios.get(Url).then((res) =>{
-        console.log(res);
-        setCoin(res.data)
-        console.log(coins);
-    }).catch((error) => {
-        console.log(error);
-    })
-}, [])
+    useEffect(() => {
+        handleCoinfetch()
+    }, [])
 
 
     return (
-        <div className='coin-header'> 
+        <div className='coin-header'>
+            
             <h2>Authorized Coins with current rate</h2>
             <div className='table-header'>
                 <ul>
@@ -37,13 +40,13 @@ useEffect(() => {
                 </ul>
             </div>
 
-{
-    coins.map((coin) =>{
-        return <Coinsitem coin ={coin} key={coin.market_cap_rank}/>;
-    })
-}
-            
-        
+            {
+                coins.map((coin) => {
+                    return <Coinsitem singleCoin={coin} key={coin.market_cap_rank} />;
+                })
+            }
+
+
         </div>
     )
 }
--- src/Components/Coins/Coinsitem.jsx
+++ src/Components/Coins/Coinsitem.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import './coins.css'
 
-const Coinsitem = ({coin}) => {
+const Coinsitem = ({singleCoin}) => {
   return (
     <div className='coin-header'> 
-            <div className='table-header'>
+            <div className='table-header container'>
                 <ul>
-                    <li>{coin.market_cap_rank}</li>
-                    <li><img src={coin.image} /> {coin.symbol}</li>
-                    <li>${coin.current_price}</li>
-                    <li>{coin.market_cap_change_percentage_24h}%</li>
-                    <li>${coin.total_volume}</li>
-                    <li>${coin.market_cap}</li>
+                    <li>{singleCoin.market_cap_rank}</li>
+                    <li><img src={singleCoin.image} /> {singleCoin.symbol}</li>
+                    <li>${singleCoin.current_price}</li>
+                    <li>{singleCoin.price_change_percentage_24h.toFixed(2)}%</li>
+                    <li>${singleCoin.total_volume}</li>
+                    <li>${singleCoin.market_cap}</li>
                 </ul>
             </div>
        
